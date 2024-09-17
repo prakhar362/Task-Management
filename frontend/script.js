d5838ddcf596abf687d3a982f2843b4d0e5388d8
prakhar362 <prakharshri2005@gmail.com>
2024-09-17 19:27:49 +0530

    Update script.js

--- frontend/script.js
+++ frontend/script.js
@@ -4,7 +4,7 @@ const closePopup = document.getElementById('closePopup');
 let selectedSection;
 let currentTaskCard = null;
 let currentTaskId = null; // Track whether we're adding a new task or editing an existing one
-const BACKEND_URL = 'https://task-management-three-swart.vercel.app'; // Replace with actual backend
+const BACKEND_URL = 'https://task-management-three-swart.vercel.app/'; // Replace with actual backend
 
 // Fetch all tasks from the server when the page loads
 window.addEventListener('load', () => {
